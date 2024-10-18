a60fd89ed6fd2b157ca64bb90d397ace6b4ec92f
= <=>
2024-10-18 09:10:39 +0200

    Initial commit

---
+++ src/test/java/com/alvaro/stockControl/StockControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaro.stockControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
